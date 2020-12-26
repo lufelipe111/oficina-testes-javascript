0b82ac0781e96a7af51f30873e612c3d4687b2b7
Luiz <lufelipe111@gmail.com>
2020-12-26 17:57:21 -0300

    studies resolution

---
+++ src/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzbuzz(numero) {
+  let resultadoFinal = ""
+
+  if (numero === 0) return numero
+
+  if (numero % 3 === 0) {
+    resultadoFinal = "Fizz"
+  }
+
+  if (numero % 5 === 0) {
+    resultadoFinal += "Buzz"
+  }
+
+  if(resultadoFinal === "") return numero
+
+  return resultadoFinal;
+}
+
+module.exports = fizzbuzz
